2878a3d2a08f86fe322a2456ce12e8d9f10c4202
Kirill <kirill-prohor@yandex.ru>
2020-08-14 15:02:47 +0300

    Add modal

--- app/js/script.js
+++ app/js/script.js
@@ -33,3 +33,58 @@ $("#carousel").waterwheelCarousel({
     sizeMultiplier: 0.7
 });
   
+// Modal
+$(".modal").each( function(){
+	$(this).wrap('<div class="overlay"></div>')
+});
+
+$(".open-modal").on('click', function(e){
+	e.preventDefault();
+	e.stopImmediatePropagation;
+
+	
+	var $this = $(this),
+			modal = $($this).data("modal");
+	
+	if (!$(modal).parents(".overlay").find("footer").is(".footer")){
+		let clone = $(".footer").clone().appendTo(".overlay");
+	}
+	
+	$(modal).parents(".overlay").addClass("open");
+	setTimeout( function(){
+		$(modal).addClass("open");
+	}, 350);
+	
+	$(document).on('click', function(e){
+		var target = $(e.target);
+		
+		if ($(target).hasClass("overlay")){
+			$(target).find(".modal").each( function(){
+				$(this).removeClass("open");
+			});			
+			setTimeout( function(){
+				$(target).removeClass("open");
+				$(target).find(".footer").remove();
+			}, 350);
+			
+		}
+		
+	});
+	
+});
+
+$(".close-modal").on('click', function(e){
+	e.preventDefault();
+	e.stopImmediatePropagation;
+	
+	var $this = $(this),
+			modal = $($this).data("modal");
+	
+	$(modal).removeClass("open");
+	setTimeout( function(){	
+		$(modal).parents(".overlay").removeClass("open");
+		$(modal).parents(".overlay").find(".footer").remove();
+	}, 350);
+	
+	
+});	
